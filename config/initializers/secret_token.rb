42f5fc2f9903117c5f29aa07944a896046865018
Jeff Bellucci + Ben Seligman <jeffmbellucci@gmail.com + seligman.ben@gmail.com>
2013-08-09 09:10:52 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicInventory::Application.config.secret_token = 'a09c54eedd7f10f90e91775fca9a10067d18cf98e7c0b746c6e920e06b8ae71458f22a21d176fe4cae5acb7122b2b4eca7de6743dbbcfc07683429cdb7b61931'
